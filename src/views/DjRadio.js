ab6286719fd6dc574ef73b288dfa3a6a1922a62e
天下大雨 <851298395@qq.com>
2017-08-09 00:17:24 +0800

    新增电台详情页面

--- src/views/DjRadio.js
+++ src/views/DjRadio.js
@@ -1,7 +1,50 @@
 import React, { Component} from 'react';
+import qs from 'query-string'
+import  * as api from '../api';
+import {dateFormat} from '../util/date'
+import {numberFormat} from '../util'
+import {Link} from 'react-router-dom'
+import {Spin} from 'antd'
 
 class DjRadio extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			djRadio:null,
+			programs:[],
+			order:0
+		}
+	}
+	componentDidMount() {
+		const id = qs.parse(this.props.location.search).id;
+		api.getDjDetail(id).then(res => {
+			// console.log(res)
+			if(res.data.code == 200) {
+
+				this.setState({
+					djRadio:res.data.djRadio
+				})
+				return api.getDjPrograms(id,0,999)
+			}
+		})
+		.then(res => {
+			if(res.data.code == 200) {
+				this.setState({
+					programs:res.data.programs
+				})
+			}
+		})
+		api.getSimiDj(id).then(res => {
+			console.log(res)
+		})
+	}
 	render() {
+		const {djRadio,programs,order} = this.state;
+		if(!djRadio) {
+			return <div className="g-bd4 clearfix">
+  						<div style={{height:(document.body.clientHeight-105)+'px'}} className="loading"><Spin tip="Loading..." /></div>
+  					</div>
+		}
 		return (
 		<div className="g-bd4 f-cb">
 			<div className="g-mn4">
@@ -9,7 +52,7 @@ class DjRadio extends Component {
 					<div className="g-wrap6">
 						<div className="m-info f-cb">
 							<div className="cover u-cover u-cover-dj">
-								<img src="http://p1.music.126.net/pdz7v8VijQEz-79JhVb_4A==/19123805742064659.jpg?param=200y200" />
+								<img src={djRadio.picUrl} />
 								<span className="msk"></span>
 							</div>
 							<div className="cnt">
@@ -17,34 +60,32 @@ class DjRadio extends Component {
 									<div className="hd f-cb">
 										<i className="f-fl u-icn u-icn-93"></i>
 										<div className="tit">
-											<h2 className="f-ff2">冯提莫</h2>
+											<h2 className="f-ff2">{djRadio.name}</h2>
 										</div>
 									</div>
 									<div className="user f-cb">
-										<a href="/user/home?id=109284159" className="face">
-											<img src="http://p1.music.126.net/NFfFES_SpseHRrpPK_FqNA==/18779658604329158.jpg?param=50y50" />
-										</a>
+										<Link to={`/user/home?id={djRadio.dj.userId}`} className="face">
+											<img src={djRadio.dj.avatarUrl} />
+										</Link>
 										<span className="name">
-											<a className="s-fc7" href="/user/home?id=109284159" title="冯提莫">冯提莫</a>
+											<Link className="s-fc7" to={`/user/home?id={djRadio.dj.userId}`} title={djRadio.dj.nickname}>{djRadio.dj.nickname}</Link>
 										</span>
 										<sup className="icn u-icn2 u-icn2-music2 "></sup>
 									</div>
 									<div className="btns f-cb">
 										<a href="" className="u-btni u-btni-sub">
-											<i>订阅(261万)</i>
+											<i>订阅({numberFormat(djRadio.subCount)})</i>
 										</a>
 										<a href="" className="u-btni u-btni-playall">
 											<i>播放全部</i>
 										</a>
 										<a href="" className="u-btni u-btni-share">
-											<i>分享(5426)</i>
+											<i>分享({numberFormat(djRadio.shareCount)})</i>
 										</a>
 									</div>
 									<p className="intr f-brk">
-									<a href="/discover/djradio/category?id=2001" className="cat cat u-type u-type-red">创作|翻唱</a>
-									Douyu：https://www.douyu.com/71017<br/>
-									sina weibo：@冯提莫<br/>
-									wei xin公众平台：fengtimo520<br/>
+									<Link to={`/discover/djradio/category?id=${djRadio.categoryId}`} className="cat cat u-type u-type-red">{djRadio.category}</Link>
+									<span dangerouslySetInnerHTML={{__html:djRadio.desc.replace(/\n/g,'<br />')}}></span>
 									</p>
 								</div>
 							</div>
@@ -52,17 +93,17 @@ class DjRadio extends Component {
 						<div className="n-songtb">
 							<div className="u-title u-title-1 f-cb">
 								<h3 id="programlist"><span className="f-ff2">节目列表</span></h3>
-								<span className="sub s-fc4">共44期</span>
+								<span className="sub s-fc4">共{djRadio.programCount}期</span>
 								<div className="u-sort f-fr f-cb">
 									<a href="" className="desc" title="降序"></a>
-									<a href="" className="asc z-sel" title="升序"></a>
+									<a href="" className="asc sel" title="升序"></a>
 								</div>
 								<div className="out f-fr">
 									<i className="u-icn u-icn-95"></i>&nbsp; 
-									<a className="s-fc7" href="/outchain/4/5811013/">生成外链播放器</a>
+									<a className="s-fc7" href="javascript:;">生成外链播放器</a>
 								</div>
 							</div>
-							<SongList />
+							<ProgramList programs={programs} order={order} />
 						</div>
 					</div>
 				</div>
@@ -118,15 +159,19 @@ class DjRadio extends Component {
 	}
 }
 
-//音乐列表
-class SongList extends Component {
+//节目列表
+class ProgramList extends Component {
 	render() {
+		const {programs,order} = this.props;
+		if(!programs.length) {
+			return null;
+		}
 		return (
 				<div className="track-list">
 					<table className="m-table m-table-program">
 						<tbody>
 							{
-							Array(50).fill(1).map((i,index) => (
+							programs.map((i,index) => (
 								<tr key={index} className={index%2 == 0?'even':null}>
 									<td className="col1">
 										<div className="hd">
@@ -135,7 +180,7 @@ class SongList extends Component {
 										</div>
 									</td>
 									<td className="col2">
-										<div className="tt f-thide"><a href="/program?id=14788078" title="IF YOU - 冯提莫">IF YOU - 冯提莫</a></div>
+										<div className="tt f-thide"><Link to={`/program?id=${i.id}`} title={i.name}>{i.name}</Link></div>
 										<div className="opt hshow">
 											<a href="javascript:;" className="u-icn u-icn-81 icn-add"></a>
 											<span className="icn icn-share"></span>
@@ -143,12 +188,12 @@ class SongList extends Component {
 										</div>
 									</td>
 									<td className="col3">
-										<span className="s-fc3">播放2310万</span>
+										<span className="s-fc3">播放{numberFormat(i.listenerCount)}</span>
 									</td>
-									<td className="col4"><span className="s-fc3">赞67492</span></td>
-									<td className="col5"><span className="s-fc4">2015-12-03</span></td>
+									<td className="col4"><span className="s-fc3">赞{i.likedCount}</span></td>
+									<td className="col5"><span className="s-fc4">{dateFormat(i.createTime,'yyyy-MM-dd')}</span></td>
 									<td className="f-pr">
-										<span className="s-fc4">04:22</span>
+										<span className="s-fc4">{dateFormat(i.duration,'mm:ss')}</span>
 									</td>
 								</tr>
 							))
